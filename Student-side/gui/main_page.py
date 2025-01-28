07f0ffbda23dde705ebe6313a7957933e824d18f
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-28 12:01:14 +0330

    user data added to user detail textbox

--- Student-side/gui/main_page.py
+++ Student-side/gui/main_page.py
@@ -1,13 +1,13 @@
-from customtkinter import CTk, CTkTextbox, CTkLabel, CTkButton, CTkFrame, CTkOptionMenu, CTkEntry
+from customtkinter import CTk, CTkTextbox, CTkLabel, CTkButton, CTkFrame, CTkOptionMenu, CTkEntry, END, DISABLED, NORMAL
 import cv2
 from PIL import Image, ImageTk
 import os
 from threading import Thread
 
 class MainPage(CTk):
-    def __init__(self):
+    def __init__(self, udata):
         super().__init__()
-
+        self.udata = udata
         self.title('Main-Page')
         self.geometry('1000x650')
         self.minsize(1000, 650)
@@ -22,7 +22,7 @@ class MainPage(CTk):
         self.elements_frame.place(relx=0.5, rely=0.5, anchor='center')
         # elements
         self.camera_label = CTkLabel(master=self.elements_frame, text='')
-        self.user_detail_textbox = CTkTextbox(master=self.elements_frame, border_color='white', border_width=2)
+        self.user_detail_textbox = CTkTextbox(master=self.elements_frame, border_color='white', border_width=2, font=('montserrat', 15, 'bold'))
         self.camera_selectbox_lbl = CTkLabel(self.elements_frame, text='Camera :' , font=('montserrat', 30, 'bold'))
         self.camera_selectbox = CTkOptionMenu(self.elements_frame, values=list(self.available_camera.keys()), font=('montserrat', 25, 'bold'), height=40, command=self.change_camera)
         self.rechech_availale_camera = CTkButton(self.elements_frame, text='Recheck', font=('montserrat', 20, 'bold'), height=40, border_color='white', border_width=2, command=self.recheck_button)
@@ -32,7 +32,11 @@ class MainPage(CTk):
         self.current_ping_status_entry = CTkEntry(self.elements_frame , font=('montserrat', 20, 'bold'), border_color='white', border_width=2)
         self.start_button = CTkButton(self.elements_frame, height=50, text='START', font=('montserrat', 20, 'bold'))
 
-        
+        # adding user data (self.udata) to textbox
+        self.user_detail_textbox.delete(1.0, END)
+        self.text = f'Name : {self.udata[0]}\nFamily : {self.udata[1]}\nClass : {self.udata[2]}'
+        self.user_detail_textbox.insert(1.0, text=self.text)
+        self.user_detail_textbox.configure(state=DISABLED)
         # placing elements
         self.camera_label.grid(        row=0, column=0, rowspan=4)
         self.user_detail_textbox.grid( row=0, column=1, padx=(20,0),   sticky='nswe', columnspan=2)
@@ -91,8 +95,8 @@ class MainPage(CTk):
     def run(self):
         self.mainloop()
 
-def main_page_func():
-    app = MainPage()
+def main_page_func(udata):
+    app = MainPage(udata)
     app.run()
 
 
