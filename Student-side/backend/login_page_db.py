fbcc3a39cf0678fe47ecdcb791278e95aaa2fe26
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-01-28 11:59:47 +0330

    returning user data added

--- Student-side/backend/login_page_db.py
+++ Student-side/backend/login_page_db.py
@@ -1,40 +1,20 @@
-# from getpass import getpass
-# from mysql.connector import connect, Error
-
-# try:
-#     with connect(
-#         host="localhost",
-#         user='root',
-#         password='ardbms',  # getpass("Enter password: ")
-#     ) as connection:
-#         print(connection)
-#         create_db_query = "CREATE DATABASE sap"
-#         with connection.cursor() as cursor:
-#             cursor.execute(create_db_query)
-# except Error as e:
-#     print(e)
-
-
-
-
 import sys
 from pathlib import Path
 # adding folder to path
 parent_dir = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(parent_dir / "database-code"))
-# sys.path.append(str(parent_dir / "Students-side/gui"))
-# print(Path(__file__).resolve().parent.parent)
 
-from searching import search_value
+from searching import search_value # type: ignore
 
 
 def check_auth(username, password):
     print('entered to check auth')
     result = search_value(value=username)
+    print(f'this is result {result}')
     print(f'search result is {result}')
     print(f'getted password is {password}')
-    if result == password:
-        return True
+    if result[2] == password:
+        return [True, result]
     else:
         return False
 
